5cd289ebb95713fd71d62968b4a5d22573b957c2
yuyunzhi <lambert1129@foxmail.com>
2021-03-04 17:36:37 +0800

    feat: add stack index

---
+++ stack/index.js
@@ -0,0 +1,13 @@
+// 先进 后出 ，后进先出
+const stack = []
+
+stack.push(1)
+stack.push(2)
+stack.push(3)
+stack.push(4)
+
+const item1 = stack.pop()
+console.log('item1',item1);
+
+const item2 = stack.pop()
+console.log('item2',item2)
