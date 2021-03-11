3ad8e043fe405fd9382e55dcd676bc12f5c13dc1
yuyunzhi <lambert1129@foxmail.com>
2021-03-11 11:54:21 +0800

    feat: update

--- array/实现一个add方法.js
+++ array/实现一个add方法.js
@@ -5,19 +5,15 @@
 
 
 function add (...args){
-
    let x =  (...args2)=>{
      return add(...args,...args2)
    }
    x.value = function (){
-     console.log(args);
      return Array.from(args).reduce((pre,cur)=>{
        return pre+cur
      },0)
    }
-
    return x
-
 }
 
 console.log(add(3)(1,2, 3)(4,8)(5).value());
