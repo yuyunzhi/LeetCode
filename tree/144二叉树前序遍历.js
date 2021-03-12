ce56201e91f6a5d59731fb75a328accfd01a287c
yuyunzhi <lambert1129@foxmail.com>
2021-03-12 10:25:30 +0800

    feat: 144二叉树前序遍历

--- tree/144二叉树前序遍历.js
+++ tree/144二叉树前序遍历.js
@@ -19,5 +19,40 @@ var preorderTraversal = function(root) {
   }
   preorder(root)
   return result
-};
+}
+
+
+
+// 前序遍历
+
+const x = (root) =>{
+  if(!root) return
+
+  console.log(root.val)
+  x(root.left)
+  x(root.right)
+}
+
+
+const preorderTraversal = (root) =>{
+    const result = []
+    const x = (root)=>{
+      const stack = [root]
+
+      while(stack.length){
+        const n = stack.pop()
+        result.push(n.val)
+        if(n.right) stack.push(n.right)
+        if(n.left) stack.push(n.left)
+      }
+    }
+    x(root)
+    return result
+}
+
+
+
+
+
+
 
