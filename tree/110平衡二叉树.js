da5248367801b9d68402cb7638e7ed34396de423
yuyunzhi <lambert1129@foxmail.com>
2021-03-15 14:59:20 +0800

    feat: 110平衡二叉树

---
+++ tree/110平衡二叉树.js
@@ -0,0 +1,20 @@
+var isBalanced = function(root) {
+  let result = true
+  const dfs = (root) => {
+
+    if(!root) return 0
+
+    let left = dfs(root.left)
+    let right = dfs(root.right)
+
+    if(Math.abs(left-right)>1){
+      result = false
+    }
+    return Math.max(left,right) + 1
+  }
+
+  dfs(root)
+
+  return result
+};
+
