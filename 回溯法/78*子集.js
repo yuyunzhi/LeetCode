b159c80a9fae0b7287a84af3850c1a9b1c9cfd45
yuyunzhi <lambert1129@foxmail.com>
2021-03-19 16:58:03 +0800

    feat: 78子集

---
+++ 回溯法/78*子集.js
@@ -0,0 +1,23 @@
+
+let subset = (nums)=>{
+  let result = []
+
+  const backtrack = (path,l,start)=>{
+      if(path.length === l){
+        result.push(path)
+        return
+      }
+      for(let i = start;i<nums.length;i++){
+        backtrack(path.concat(nums[i]),l,i+1)
+      }
+  }
+
+  for(let i = 0;i<nums.length;i++){
+    backtrack([],i,0)
+  }
+
+  return result
+}
+
+
+console.log(subset([1, 2, 3]));
