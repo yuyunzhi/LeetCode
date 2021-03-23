a9036ca5d6bcd7d3f10daeac21e5bfbed5c4b21c
yuyunzhi <lambert1129@foxmail.com>
2021-03-23 17:45:41 +0800

    feat: add bubblesort

---
+++ sort/bubbleSort.js
@@ -0,0 +1,16 @@
+Array.prototype.bubbleSort = function (){
+
+  for(let i = 0; i<this.length-1;i++){
+    for(let j = 0;j<this.length -1-i;j++){
+      if(this[j]>this[j+1]){
+        [this[j],this[j+1]] = [this[j+1],this[j]]
+      }
+    }
+  }
+  return this
+}
+
+const arr = [5,4,3,2,1]
+console.log(arr.bubbleSort());
+
+// 时间复杂度 O(n)
