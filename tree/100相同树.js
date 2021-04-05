da9de8262e49638f019df464dc4af17a86ecd79a
yuyunzhi <lambert1129@foxmail.com>
2021-04-05 16:31:00 +0800

    feat: 100相同树

---
+++ tree/100相同树.js
@@ -0,0 +1,9 @@
+var isSameTree = function(p, q) {
+  if(!q && !p) return true
+
+  if(q && p && q.val === p.val && isSameTree(p.left,q.left) && isSameTree(p.right,q.right)){
+    return true
+  }
+
+  return false
+};
