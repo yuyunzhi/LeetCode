7b07f9095c0699f233a4984d8ebfa6f2039002cd
yuyunzhi <lambert1129@foxmail.com>
2021-04-05 15:16:06 +0800

    feat: sort merge

--- sort/mergeSort.js
+++ sort/mergeSort.js
@@ -4,19 +4,34 @@ Array.prototype.mergeSort = function (){
       const mid = Math.floor(arr.length/2)
       const left = arr.slice(0,mid)
       const right = arr.slice(mid,arr.length)
+
       const orderLeft = rec(left)
       const orderRight = rec(right)
+
       const res = []
-      while(orderLeft.length || orderRight.length){
-        if(orderLeft.length && orderRight.length){
+
+      while(orderLeft.length || orderRight.length) {
+
+        if (orderLeft.length && orderRight.length) {
           res.push(orderLeft[0] < orderRight[0] ? orderLeft.shift() : orderRight.shift())
+        } else if (orderLeft.length) {
+          res.push(orderLeft.shift())
+        } else if (orderRight.length) {
+          res.push(orderRight.shift())
         }
       }
+
+      return res
   }
 
-  rec(this)
+   const res =  rec(this)
+  res.forEach((i,index)=>{
+      this[index] = i
+    })
+
+  return res
 }
 
 
-const arr = [5, 4, 3, 2, 1]
-console.log(arr.mergeSort());
+const arr = [5, 4,3,2,1]
+console.log(arr.mergeSort(),arr);
