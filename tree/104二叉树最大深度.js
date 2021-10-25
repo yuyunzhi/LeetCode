adc5797f1f21150bd3c1afe9acfc2723cc1db12a
yuyunzhi <lambert1129@foxmail.com>
2021-10-25 14:12:12 +0800

    feat: update tree

--- tree/104二叉树最大深度.js
+++ tree/104二叉树最大深度.js
@@ -1,17 +1,16 @@
-
-const maxDeep = (root)=>{
+const maxDeep = (root) => {
   let maxL = 0
 
-  const dfs = (n,l)=>{
-      if(!n) return
-    if(!n.right && !n.left) {
-      maxL = Math.max(maxL,l)
+  const dfs = (n, l) => {
+    if (!n) return
+    if (!n.right && !n.left) {
+      maxL = Math.max(maxL, l)
     }
-      dfs(n.left,l+1)
-      dfs(n.right,l+1)
+    dfs(n.left, l + 1)
+    dfs(n.right, l + 1)
   }
 
-  dfs(root,1)
+  dfs(root, 1)
 
   return maxL
 }
